2b82213a78bae56e346a21e1294fd5ce141b549f
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2013-07-19 14:46:58 -0700

    Show timezones in the example

--- examples/calendar.rb
+++ examples/calendar.rb
@@ -13,8 +13,10 @@ cal = Kali::Calendar.new do |calendar|
     event.geo = [-54.458594, -34.123310]
     event.categories = ["CATEGORY A", "CATEGORY B"]
 
-    event.dtstart = DateTime.new(2013, 7, 31, 15, 00, 00)
-    event.dtend = DateTime.new(2013, 7, 31, 16, 00, 00)
+    event.dtstart = DateTime.civil(2013, 7, 31, 15, 00, 00, "-07:00")
+    event.dtstart.tzid = "America/Los_Angeles"
+    event.dtend = DateTime.civil(2013, 7, 31, 16, 00, 00, "-07:00")
+    event.dtend.tzid = "America/Los_Angeles"
 
     event.comments.add "I'm sure the talk will be a disaster --The Speaker"
     event.comments.add "Estoy convencido de que va a ser buena" do |comment|
@@ -37,7 +39,8 @@ cal = Kali::Calendar.new do |calendar|
     event.summary = "Another Thing"
     event.location = "The Venue"
 
-    event.dtstart = DateTime.new(2013, 7, 31, 18, 00, 00)
+    event.dtstart = DateTime.new(2013, 7, 31, 18, 00, 00, "+02:00")
+    event.dtstart["TZID"] = "Europe/Berlin"
     event.duration = { minute: 45 }
   end
 end
