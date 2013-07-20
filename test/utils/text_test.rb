16857d3da9d8abd8acfe3602ed4bb53baa424c24
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2013-07-20 14:28:09 -0700

    Fix the tests after removing \r from line folding

--- test/utils/text_test.rb
+++ test/utils/text_test.rb
@@ -16,19 +16,19 @@ class Kali::Utils::TextTest < MiniTest::Unit::TestCase
   def test_fold_line_into_two_line_string
     line = "a" * 120
     folded = Kali::Utils::Text.fold_line(line)
-    expected = "#{"a" * 75}\r\n #{"a" * 45}"
+    expected = "#{"a" * 75}\n #{"a" * 45}"
     assert_equal expected, folded
   end
 
   def test_fold_line_into_three_line_string
     line = "a" * 200
     folded = Kali::Utils::Text.fold_line(line)
-    expected = "#{"a" * 75}\r\n #{"a" * 74}\r\n #{"a" * 51}"
+    expected = "#{"a" * 75}\n #{"a" * 74}\n #{"a" * 51}"
     assert_equal expected, folded
   end
 
   def test_unfold_long_line
-    lines = "#{"a" * 75}\r\n #{"a" * 74}\r\n #{"a" * 51}"
+    lines = "#{"a" * 75}\n #{"a" * 74}\n #{"a" * 51}"
     actual = Kali::Utils::Text.unfold_line(lines)
     expected = "a" * 200
     assert_equal expected, actual
